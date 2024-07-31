251b6340eb252a4be37f87486848f8fe99960c9e
leoversion <ayushpandey939@gmail.com>
2024-07-31 20:17:55 +0530

    recreated socketIo connection

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,17 @@
 const path = require("path")
 const express = require("express");
+const { createServer } = require("http");
+
 const app = express();
-require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const httpServer = createServer(app);
+
 const cors = require("cors");
+
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
 const connectDB = require("../config/db");
 
 app.use(cors({
@@ -15,7 +23,6 @@ app.use(express.json());
 const userRoute = require("../routes/userRoute");
 const chatRoute = require("../routes/chatRoute");
 const messageRoute = require("../routes/messageRoute");
-const { socketConnect } = require("../config/webSocketConnect");
 
 app.use("/api/user",userRoute);
 app.use("/api/chats",chatRoute);
@@ -27,13 +34,11 @@ app.get('/',(req,res)=>{
 
 connectDB();
 
-const server = app.listen(PORT ,"0.0.0.0", ()=>{
-    console.log(`server is running on http://localhost:${PORT}`);
-})
-
 // ------------------------socket.io for live chatting------------------------------------
+const { Server } = require("socket.io");
+const { socketConnect } = require("../config/webSocketConnect");
 
-const io = require('socket.io')(server, {
+const io = new Server(httpServer, {
     pingTimeout: 60000, // in milliseconds
     cors: {
         origin: 'https://gossip-a-mern-chat-app.vercel.app',
@@ -44,3 +49,7 @@ const io = require('socket.io')(server, {
 io.on("connection", socketConnect)
 
 // ------------------------socket.io for live chatting------------------------------------
+
+httpServer.listen(PORT ,"0.0.0.0", ()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+})
