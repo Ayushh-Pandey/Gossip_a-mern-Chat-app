1bf8ab8b54d74e8af3b10f083e9717d0e1e4ac52
leoversion <ayushpandey939@gmail.com>
2024-07-31 20:32:39 +0530

    commit

--- server/api/index.js
+++ server/api/index.js
@@ -36,7 +36,6 @@ connectDB();
 
 // ------------------------socket.io for live chatting------------------------------------
 const { Server } = require("socket.io");
-const { socketConnect } = require("../config/webSocketConnect");
 
 const io = new Server(httpServer, {
     pingTimeout: 60000, // in milliseconds
@@ -46,7 +45,47 @@ const io = new Server(httpServer, {
         credentials: true
     }
 });
-io.on("connection", socketConnect)
+io.on("connection", (socket) => {
+    console.log("connected to socket.io")
+
+    socket.on('setup', (userData) => {
+        socket.join(userData._id);
+        console.log(userData._id);
+        socket.emit('connected')
+    })
+
+    socket.on('join_chat', (room) => {
+        socket.join(room);
+        console.log('room', room)
+    })
+
+    socket.on('typing', (room) =>
+        socket.in(room).emit("typing")
+    )
+    socket.on('stop_typing', (room) =>
+        socket.in(room).emit("stop_typing")
+    )
+
+    socket.on('new_message', (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+
+        if (!chat.users)
+            return console.log("chat.users not defined")
+
+        chat.users.forEach((user) => {
+            if (user._id == newMessageReceived.sender._id)
+                return;
+
+            socket.in(user._id).emit('message_received', newMessageReceived);
+
+        })
+    })
+
+    socket.off('setup', () => {
+        console.log("USER DISCONNECTED");
+        socket.leave(userData._id);
+    })
+})
 
 // ------------------------socket.io for live chatting------------------------------------
 
